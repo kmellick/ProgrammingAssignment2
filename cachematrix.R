b5c1d0884e83744ac9605d0f8eccd7506983de95
Kmellickson <kristie.ellickson@state.mn.us>
2015-01-22 17:26:34 -0600

    Modified cachematrix.R to calculate inverse of a matrix first checking if previously run.
    This accomplishes the goal of Programming Assignment 2 in the Course R Programming Course.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will calculate the inverse of a given matrix, checking if ##that inversion calculation has already been run. If already run, the ##inversion results will have been cached.
 
-## Write a short comment describing this function
 
+## The makeCacheMatrix function first sets arguments to NULL if the second function has not yet been run. Second part of this function creates a list of functions to provide order for the second function. The functions in this list set values of a matrix, caches the matrix that is input to check against an output.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
 
+## The cacheSolve function checks if the inverse of a matrix has already ben calculated, and if so returns message text.If this function does not find a previously existing inverse matrix, an inverse is calculated.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached inverse matrix solution")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
